4bf3873b9d19c0b13a94827220189c0766ff4cc2
Aman Mittal <amandeepmittal@live.com>
2019-07-10 19:05:28 +0530

    getting started

--- App.js
+++ App.js
@@ -1,5 +1,24 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import {
+	NoteOne,
+	NoteTwo,
+	NoteThree,
+	NoteFour,
+	NoteFive,
+	NoteSix,
+	NoteSeven
+} from './constants/Colors'
+
+const xyloSounds = {
+	one: require('./assets/sounds/note1.wav'),
+	two: require('./assets/sounds/note2.wav'),
+	three: require('./assets/sounds/note3.wav'),
+	four: require('./assets/sounds/note4.wav'),
+	five: require('./assets/sounds/note5.wav'),
+	six: require('./assets/sounds/note6.wav'),
+	seven: require('./assets/sounds/note7.wav')
+}
 
 export default function App() {
 	return (
---
+++ constants/Colors.js
@@ -0,0 +1,7 @@
+export const NoteOne = 'red'
+export const NoteTwo = 'orange'
+export const NoteThree = 'yellow'
+export const NoteFour = 'green'
+export const NoteFive = '#00FFFF	'
+export const NoteSix = '#000080'
+export const NoteSeven = '#B266FF'
